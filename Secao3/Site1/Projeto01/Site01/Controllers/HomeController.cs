4c277fcee55f058bb1d8122690a72c0670b61cc6
tarcisiokta2012@gmail.com <tarcisiokta2012@gmail.com>
2022-10-04 12:13:58 -0300

    Begin with MVC

---
+++ Secao3/Site1/Projeto01/Site01/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Site01.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            // return new ContentResult() { Content = "Hello world"};
+            return View();
+        }
+
+        public string Test()
+        {
+            return "Test!";
+        }
+    }
+}
